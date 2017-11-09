f5f5703dd81e13d14b184f4d79f8975c71a61fac
Jiande Wu <jwu2@lsuhsc.edu>
2017-11-09 16:16:37 -0600

    a faster version

--- gene_match/genematch.cpp
+++ gene_match/genematch.cpp
@@ -8,69 +8,88 @@
 #include "alphanum_comp.h"
 using namespace std;
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
+    struct Gene_ref
+    {
+        string gene_id;
+        string chromosome;
+        size_t start_bp;
+        size_t end_bp;
+        string gene_name;
+        Gene_ref(string s1, string s2, size_t s3, size_t s4, string s5) : gene_id(s1), chromosome(s2), start_bp(s3), end_bp(s4), gene_name(s5) {}
+    };
+
     // heterogeneous comparison:
     struct Comp0
     {
-        bool operator() (const vector<string>& s1, const vector<string>& s2 ) const {
-            int i = doj::alphanum_comp(s1[1], s2[1]);
-            if (i != 0) return i < 0 ? true : false;
-            return stoi(s1[2]) < stoi(s2[2]);
+        bool operator()(const Gene_ref &s1, const Gene_ref &s2) const
+        {
+            int i = doj::alphanum_comp(s1.chromosome, s2.chromosome);
+            if (i != 0)
+                return i < 0 ? true : false;
+            return s1.start_bp < s2.start_bp;
         }
     };
     struct Comp1
     {
-        bool operator() ( const vector<string>& s, string str ) const { return doj::alphanum_comp(s[1], str) < 0 ? true : false; }
-        bool operator() ( string str, const vector<string>& s ) const { return doj::alphanum_comp(str, s[1]) < 0 ? true : false; }
+        bool operator()(const Gene_ref & s, string str) const { return doj::alphanum_comp(s.chromosome, str) < 0 ? true : false; }
+        bool operator()(string str, const Gene_ref & s) const { return doj::alphanum_comp(str, s.chromosome) < 0 ? true : false; }
     };
     struct Comp2
     {
-        bool operator() ( const vector<string>& s, string str ) const { return stoi(s[2]) < stoi(str); }
-        bool operator() ( string str, const vector<string>& s ) const { return stoi(str) < stoi(s[2]); }
+        bool operator()(const Gene_ref & s1, size_t s2) const { return s1.start_bp < s2; }
+        bool operator()(size_t s2, const Gene_ref & s1) const { return s2 < s1.start_bp; }
     };
 
     string inputfile = argv[1];
-    string outputfile = inputfile + "_gene2++.txt";
+    string outputfile = inputfile + "_gene++.txt";
 
     ifstream reffile("Gene_location.txt");
     string line;
-    vector<vector<string> > refgene;
-    while (getline(reffile, line)) {
+    getline(reffile, line);
+
+    vector<Gene_ref> vec_gene;
+    while (getline(reffile, line))
+    {
         istringstream iss(line);
         vector<string> tokens;
         copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(tokens));
-        refgene.push_back(tokens);
+        Gene_ref gene(tokens[0], tokens[1], stoi(tokens[2]), stoi(tokens[3]), tokens[4]);
+        vec_gene.push_back(gene);
     }
-    
-    sort(refgene.begin(),refgene.end(),Comp0());
+
+    sort(vec_gene.begin(), vec_gene.end(), Comp0());
 
     ifstream sourcefile(inputfile);
     ofstream out(outputfile);
 
     string output;
-    while (getline(sourcefile, line)) {
+    while (getline(sourcefile, line))
+    {
         istringstream iss(line);
         vector<string> tokens;
         copy(istream_iterator<string>(iss), istream_iterator<string>(), back_inserter(tokens));
-        
-        if (tokens[3] == "B") {
-            auto p = equal_range(refgene.begin(), refgene.end(), tokens[0], Comp1());
-            auto up = upper_bound(p.first, p.second, tokens[1], Comp2());
+
+        if (tokens[3] == "B")
+        {
+            size_t position = stoi(tokens[1]);
+            auto p = equal_range(vec_gene.begin(), vec_gene.end(), tokens[0], Comp1());
+            auto up = upper_bound(p.first, p.second, position, Comp2());
 
             for (auto low = p.first; low != up; ++low) {
-                if (stoi((*low)[3]) >= stoi(tokens[1])) {
-                   output += ((*low)[4] + '\t' + line + '\n');
+                if (low->end_bp >= position) {
+                   output += (low->gene_name + '\t' + line + '\n');
                 }
             }
 
             // // may faster methord
             // vector<size_t> vecinx(up - p.first);
-            // iota(vecinx.begin(), vecinx.end(), p.first - refgene.begin());
-            // sort(vecinx.begin(), vecinx.end(), [&refgene](size_t i1, size_t i2) {return stoi(refgene[i1][3]) < stoi(refgene[i2][3]);});
-            // auto upp = upper_bound(vecinx.begin(), vecinx.end(), tokens[1], [&refgene](string i2, size_t i1) {return stoi(i2) < stoi(refgene[i1][3]);});
+            // iota(vecinx.begin(), vecinx.end(), p.first - vec_gene.begin());
+            // sort(vecinx.begin(), vecinx.end(), [&vec_gene](size_t i1, size_t i2) {return vec_gene[i1].end_bp < vec_gene[i2].end_bp;});
+            // auto upp = upper_bound(vecinx.begin(), vecinx.end(), position, [&vec_gene](size_t i2, size_t i1) {return i2 < vec_gene[i1].end_bp;});
             // for (auto low = upp; low != vecinx.end(); ++low) {
-            //     output += (refgene[*low][4] + '\t' + line + '\n');
+            //     output += (vec_gene[*low].gene_name + '\t' + line + '\n');
             // }
         }
     }
